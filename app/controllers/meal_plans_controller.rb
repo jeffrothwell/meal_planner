36c4670a912c45471b21c9fd4350e4a9d0a4b062
jeffrothwell <rothwelljeff@gmail.com>
2026-06-10 13:48:35 -0400

    allow relevant controller actions to respond with json

--- app/controllers/meal_plans_controller.rb
+++ app/controllers/meal_plans_controller.rb
@@ -3,9 +3,17 @@ class MealPlansController < ApplicationController
 
   def index
     @plans = MealPlan.includes(:meals).order(week_start_date: :desc)
+    respond_to do |format|
+      format.html
+      format.json { render json: @plans.map { |p| plan_json(p) } }
+    end
   end
 
   def show
+    respond_to do |format|
+      format.html
+      format.json { render json: plan_json(@plan) }
+    end
   end
 
   def new
@@ -22,42 +30,50 @@ class MealPlansController < ApplicationController
 
   def create
     week_start_date = params[:week_start_date]
-    meal_count = params[:meal_count].to_i
-    meal_ids = params[:meal_ids]&.map(&:to_i) || []
+    meal_count      = params[:meal_count].to_i
+    meal_ids        = params[:meal_ids]&.map(&:to_i) || []
 
-    begin
-      plan = MealPlan.create!(week_start_date: week_start_date, meal_count: meal_count)
-      plan.meal_ids = meal_ids
-      redirect_to meal_plan_path(plan)
-    rescue ActiveRecord::RecordNotUnique
-      existing = MealPlan.find_by!(week_start_date: week_start_date)
-      redirect_to edit_meal_plan_path(existing)
+    respond_to do |format|
+      begin
+        plan = MealPlan.create!(week_start_date: week_start_date, meal_count: meal_count)
+        plan.meal_ids = meal_ids
+        format.html { redirect_to meal_plan_path(plan) }
+        format.json { render json: plan_json(MealPlan.includes(:meals).find(plan.id)), status: :created }
+      rescue ActiveRecord::RecordNotUnique
+        existing = MealPlan.find_by!(week_start_date: week_start_date)
+        format.html { redirect_to edit_meal_plan_path(existing) }
+        format.json { render json: { error: "A plan already exists for this week", existingPlanId: existing.id }, status: :conflict }
+      end
     end
   end
 
   def edit
-    @meals = @plan.meals
+    @meals           = @plan.meals
     @week_start_date = @plan.week_start_date
-    @meal_count = @plan.meal_count
+    @meal_count      = @plan.meal_count
   end
 
   def update
     meal_count = params[:meal_count].to_i
-    meal_ids = params[:meal_ids]&.map(&:to_i) || []
+    meal_ids   = params[:meal_ids]&.map(&:to_i) || []
 
     @plan.update!(meal_count: meal_count)
     @plan.meal_ids = meal_ids
-    redirect_to meal_plan_path(@plan)
+
+    respond_to do |format|
+      format.html { redirect_to meal_plan_path(@plan) }
+      format.json { render json: plan_json(MealPlan.includes(:meals).find(@plan.id)) }
+    end
   end
 
   def generate
-    week_start_date = Date.parse(params[:week_start_date])
-    meal_count = params[:meal_count].to_i
+    week_start_date  = Date.parse(params[:week_start_date])
+    meal_count       = params[:meal_count].to_i
     exclude_meal_ids = Array(params[:exclude_meal_ids]).map(&:to_i)
 
     meals = MealPlan.generate(
-      week_start_date: week_start_date,
-      meal_count: meal_count,
+      week_start_date:  week_start_date,
+      meal_count:       meal_count,
       exclude_meal_ids: exclude_meal_ids
     )
 
@@ -82,18 +98,27 @@ class MealPlansController < ApplicationController
   private
 
   def set_plan
-    @plan = MealPlan.find(params[:id])
+    @plan = MealPlan.includes(:meals).find(params[:id])
+  end
+
+  def plan_json(plan)
+    {
+      id:            plan.id,
+      weekStartDate: plan.week_start_date,
+      mealCount:     plan.meal_count,
+      meals:         plan.meals.map { |m| meal_json(m) }
+    }
   end
 
   def meal_json(meal)
     {
-      id: meal.id,
-      title: meal.title,
-      description: meal.description,
-      dinnerCount: meal.dinner_count,
+      id:              meal.id,
+      title:           meal.title,
+      description:     meal.description,
+      dinnerCount:     meal.dinner_count,
       nutritionRating: meal.nutrition_rating,
-      kidsRating: meal.kids_rating,
-      adultRating: meal.adult_rating
+      kidsRating:      meal.kids_rating,
+      adultRating:     meal.adult_rating
     }
   end
 end
--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -3,9 +3,17 @@ class MealsController < ApplicationController
 
   def index
     @meals = Meal.includes(meal_plan_meals: :meal_plan).order(updated_at: :desc)
+    respond_to do |format|
+      format.html
+      format.json { render json: @meals.map { |m| meal_json(m) } }
+    end
   end
 
   def show
+    respond_to do |format|
+      format.html
+      format.json { render json: meal_json(@meal) }
+    end
   end
 
   def new
@@ -14,10 +22,14 @@ class MealsController < ApplicationController
 
   def create
     @meal = Meal.new(meal_params)
-    if @meal.save
-      redirect_to @meal, notice: "Meal added."
-    else
-      render :new, status: :unprocessable_entity
+    respond_to do |format|
+      if @meal.save
+        format.html { redirect_to @meal, notice: "Meal added." }
+        format.json { render json: meal_json(@meal), status: :created }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: { errors: @meal.errors.full_messages }, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -25,17 +37,40 @@ class MealsController < ApplicationController
   end
 
   def update
-    if @meal.update(meal_params)
-      redirect_to @meal, notice: "Meal updated."
-    else
-      render :edit, status: :unprocessable_entity
+    respond_to do |format|
+      if @meal.update(meal_params)
+        format.html { redirect_to @meal, notice: "Meal updated." }
+        format.json { render json: meal_json(@meal) }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: { errors: @meal.errors.full_messages }, status: :unprocessable_entity }
+      end
     end
   end
 
   private
 
   def set_meal
-    @meal = Meal.find(params[:id])
+    @meal = Meal.includes(meal_plan_meals: :meal_plan).find(params[:id])
+  end
+
+  def meal_json(meal)
+    {
+      id:                meal.id,
+      title:             meal.title,
+      description:       meal.description,
+      nutritionRating:   meal.nutrition_rating,
+      kidsRating:        meal.kids_rating,
+      adultRating:       meal.adult_rating,
+      dinnerCount:       meal.dinner_count,
+      seasonalPreference: meal.seasonal_preference,
+      varietyGroup:      meal.variety_group,
+      recipeUrl:         meal.recipe_url,
+      lastPlanned:       meal.meal_plan_meals
+                             .max_by { |mpm| mpm.meal_plan.week_start_date }
+                             &.meal_plan
+                             &.week_start_date,
+    }
   end
 
   def meal_params
